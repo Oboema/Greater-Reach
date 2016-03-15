af1ee3410609677d9871ecc79867ec61a6ff41cb
Daniel Los <danielofdoom@gmail.com>
2016-03-15 02:25:37 +0100

    Adding the mod files to the git repo

---
+++ data.lua
@@ -0,0 +1,4 @@
+data.raw["player"]["player"].build_distance = 20
+data.raw["player"]["player"].drop_item_distance = 20
+data.raw["player"]["player"].reach_distance = 20
+data.raw["player"]["player"].reach_resource_distance = 20
